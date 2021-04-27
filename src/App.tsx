18694502c36f2ee4c440b45613faebc79b8ebc89
Slab3 <zshylga@gmail.com>
2021-04-27 20:19:24 +0300

    Created ForgotPassword component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ForgotPassword } from './components/index'
 
 function App() {
   return (
-   <p>Messenger app</p>
+    <>
+      <p>Messenger app</p>
+      <ForgotPassword/>
+    </>
   );
 }
 

52916f6ffc8e8ee74e717c96fd49eb9c74811eb0
BogdanSav <bohdan16@live.com>
2021-05-18 12:51:06 +0300

    add POST request for login

--- src/redux/saga/sagas.ts
+++ src/redux/saga/sagas.ts
@@ -1,16 +1,18 @@
 import {select,takeLeading} from "redux-saga/effects";
-import {SIGNUP_ACTION} from "../actions/actions";
+import {LOGIN, SIGNUP_ACTION} from "../actions/actions";
 
-const url : string = "/signup";
+const urlSignup : string = "/signup";
+const urlSignin: string = "/signin";
 export function* sagaWatcher(){
     yield takeLeading(SIGNUP_ACTION,signUp);
+    yield takeLeading(LOGIN,signIn);
 }
 
 function* signUp(){
     const data:object = yield select(state=>state.signup);
     yield console.log(data);
     try {
-       const response:PromiseFulfilledResult<any> = yield  fetch(url,{
+       const response:PromiseFulfilledResult<any> = yield  fetch(urlSignup,{
             method : "POST",
             headers :{
                 'Content-Type': 'application/json;charset=utf-8',
@@ -23,6 +25,25 @@ function* signUp(){
         yield console.log(response);
     }
     catch (e) {
+        console.log(e);
+    }
+}
+function* signIn(){
+    const data: object = yield select(state=>state.login);
+    try{
+        const response:PromiseFulfilledResult<any> = yield  fetch(urlSignin,{
+            method : "POST",
+            headers :{
+                'Content-Type': 'application/json;charset=utf-8',
+                "Accept": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            },
+           mode: 'no-cors',
+           body : JSON.stringify(data)
+        })
+        yield console.log(response); 
+    }
+    catch(e){
         console.log(e);
     }
 }
